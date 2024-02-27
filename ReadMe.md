78d73399a05816d115e5dfb93c6a0c6919eb35a5
dganesh22 <dganesh2022@gmail.com>
2024-02-27 11:50:01 +0530

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+### File upload API
+
+## 1. create package.json
+
+    npm init -y
+
+## 2. npm packages
+
+    npm i --save express cors dotenv mongoose multer http-status-codes
+
+multer -> express middleware used to upload files
+express-fileupload ->
+http-status-codes => to handle http status codes
---
+++ index.js
@@ -0,0 +1,45 @@
+// module imports
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+const cors = require('cors')
+const { StatusCodes } = require('http-status-codes')
+
+// port import
+const PORT = process.env.PORT
+
+// instance of express
+const app = express()
+
+// bodyparser middleware for incoming data
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// middleware
+app.use(cors())
+
+// index route
+app.get(`/`, async (req,res) => {
+    try {
+        return res.status(StatusCodes.ACCEPTED).json({ status: true, msg: `Welcome to fileupload api.`})
+    } catch (err) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false, msg: err})
+    }
+})
+
+// api route
+app.use(`/api/file`, require('./route/fileRoute'))
+
+// default route
+app.all(`**`, async (req,res) => {
+    try {
+        return res.status(StatusCodes.NOT_FOUND).json({ status: false, msg: `requested path not found`})
+    } catch (err) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false, msg: err})
+    }
+})
+
+// server listener
+app.listen(PORT,() => {
+    console.log(`server is running @ http://localhost:${PORT}`)
+})
